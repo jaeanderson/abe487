b9642ce29a6f23d11ce3e52ddd37a6c1508a19eb
Jae Richard Anderson <jaea@login2.cm.cluster>
2017-09-20 22:50:17 -0700

    complete homework/hello

--- problems/hello/hello-arg2.sh
+++ problems/hello/hello-arg2.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+#if no argument, then usage statement is printed
+if [[ $# -lt 1 ]]; then
+        printf "Usage: %s GREETING NAME\n" "$(basename "$0")"
+        exit 1
+fi
+
+GREETING=$1
+NAME=$2
+
+echo "$GREETING, $NAME!"
