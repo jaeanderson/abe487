22cbb423f40453c04acbe4f7a4217444d696fff6
Jae Richard Anderson <jaea@login3.cm.cluster>
2017-10-03 12:14:40 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "OK"
+IN_FILE=$1		#creates variable for input file as argument1
+ 
+if [[ $# -ne 1 ]]; then					#check is number of arguments is not zero
+        printf "Usage: %s file\n" "$(basename "$0")"	#outputs error message with format
+        exit 1						#exits script with error message
+fi
+
+
+if [[ ! -f $IN_FILE ]]; then				#check if input file is a correct file
+	echo "\"$IN_FILE\" is not a file"		#outputs error message if input file is not a correct file
+	exit 1						#exits script with error message
+fi
+
+i=0						#initialize counter
+while read -r FILELINE; do			#reads every line in given file
+	let i++					#increments counter by 1	
+	printf "%d %s\n" $i "$FILELINE"		#output line of file preceded by number line
+done < "$IN_FILE"
+
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "OK"
+
+FILENAME=$1		#creates variable FILENAME as argument1
+NUM_ARG=${2:-3}		#creates variable NUM_ARG as argument2 with default set to integer 3
+
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then			#checks input arguments is not zero and not greater than three
+        printf "Usage: %s FILE [NUM]\n" "$(basename "$0")"	#outputs error message with format
+        exit 1							#exits script with error message
+fi
+
+if [[ ! -f $FILENAME ]]; then					#checks given filename is a file
+#        printf "%s %s\n" "\"$FILENAME\"" "is not a file"	#output error message
+        echo "\"$FILENAME\" is not a file"
+	exit 1							#exits script with error message
+fi
+
+i=0							#initialize counter
+while read -r FILELINE; do				#reads every line in given file
+        let i++						#increments counter by 1
+	if [[ $i -gt $NUM_ARG ]]; then			#checks to see if counter is greater than given number of arguments
+		break					#exits while loop if counter is greater than number of arguments
+	else
+		printf "%s\n" "$FILELINE"		#outputs number of file lines designated by given number of arguments
+	fi
+done < "$FILENAME"
+
