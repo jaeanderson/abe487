86043e710f8e89c3678569795b8805a05657784d
Jae Richard Anderson <jaea@login3.cm.cluster>
2017-10-05 19:33:30 -0700

    completed-hello-py

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 """say hello to the arguments"""
 
-print('OK')
+import os
+import sys
+
+myarg = sys.argv[0]
+names = sys.argv[1:]
+
+if len(names) < 1:
+	script = os.path.basename(myarg)
+	print('Usage: {} NAME [NAME2...]'.format(script))
+	sys.exit(1)
+
+num_people = len(names)
+
+if num_people == 1:
+	print('Hello to the', num_people, 'of you: {}!'.format(*names))
+elif num_people == 2:
+	print('Hello to the', num_people, 'of you: {}!'.format(' and '.join(names)))
+else:
+	lastname = names.pop(-1)
+	print('Hello to the', num_people, 'of you: {}'.format(', '.join(names)) + ', and',lastname + '!')
+
+
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 """count the vowels in a word"""
 
-print('OK')
+import os
+import sys
+
+myarg = sys.argv[1:]
+
+if len(myarg) < 1:
+        script = os.path.basename(sys.argv[0])
+        print('Usage: {} STRING'.format(script))
+        sys.exit(1)
+
+word=myarg[0]
+vowel_ctr=0
+vowels='aeiou'
+for letter in word:
+	if letter in vowels:
+		vowel_ctr += 1
+
+if vowel_ctr == 1:
+	print('There is', vowel_ctr, 'vowel in', '"' + word + '."')
+else:
+	print('There are', vowel_ctr, 'vowels in', '"' +  word + '."')
+
